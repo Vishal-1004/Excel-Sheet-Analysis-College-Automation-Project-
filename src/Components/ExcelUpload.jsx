4138ef0b25cbe7b728123fa0a5c922d6d399858d
Shanky <shashanksharma03.07@gmail.com>
2024-09-18 13:52:17 +0530

    enhanced the upload component

--- src/Components/ExcelUpload.jsx
+++ src/Components/ExcelUpload.jsx
@@ -2,6 +2,10 @@ import { read, utils } from "xlsx";
 import ToastMsg from "../Utils/ToastMsg";
 
 const ExcelUpload = ({ heading, onFileChange, sheetNo }) => {
+  const MessageSheetOne = "'Registration Number', 'Question Number' and 'Marks'";
+  const MessageSheetTwo = "'Registration Number' and 'Question Number'";
+  const message = (sheetNo === 1) ? MessageSheetOne : MessageSheetTwo;
+
   const handleFileChange = async (event) => {
     const file = event.target.files[0];
     let success = false;
@@ -56,7 +60,7 @@ const ExcelUpload = ({ heading, onFileChange, sheetNo }) => {
   };
 
   return (
-    <div className="border-2 mt-4 border-gray-200 p-5 rounded-lg text-center w-[90vw] sm:w-1/2 md:w-2/3 md:h-[220px] mx-auto shadow-md">
+    <div className="border-2 mt-4 border-gray-200 p-5 rounded-lg text-center w-[80vw] sm:w-3/4 md:w-4/5 h-[280px] mx-auto shadow-md">
       <h2 className="text-xl font-semibold mb-4">{heading}</h2>
 
       <div className="relative inline-block w-full">
@@ -69,10 +73,12 @@ const ExcelUpload = ({ heading, onFileChange, sheetNo }) => {
         />
         <label
           htmlFor={`file-upload-${heading}`}
-          className="block cursor-pointer border-2 border-dashed border-gray-300 rounded-md p-4 hover:bg-gray-100 transition"
+          className="block cursor-pointer border-2 border-dashed border-gray-300 rounded-md p-4 hover:bg-gray-100 transition h-[200px] md:h-auto flex flex-col items-center justify-center"
         >
-          Browse the Excel files you want to upload from your computer or drag
-          and drop
+          <p className="text-gray-500 mb-4 text-[15px]">
+            Ensure the file contains the following columns: <br />
+            <strong>{message}</strong>
+          </p>
           <div className="bg-green-700 text-white rounded-full w-8 h-8 flex items-center justify-center mt-2 mx-auto">
             +
           </div>
